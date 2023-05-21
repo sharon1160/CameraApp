55113782d0ab7c43006694fb6f5953afaff9bf66
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-21 16:51:30 -0500

    chore: add R library

--- app/src/main/java/com/example/cameraapp/view/ui/theme/Type.kt
+++ app/src/main/java/com/example/cameraapp/view/ui/theme/Type.kt
@@ -7,13 +7,14 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.cameraapp.R
 
 
 val NotoSerif = FontFamily(
-    Font(font.noto_serif_regular, FontWeight.Normal),
-    Font(font.noto_serif_bold, FontWeight.Bold),
-    Font(font.noto_serif_italic, FontWeight.Light, FontStyle.Italic),
-    Font(font.noto_serif_bold_italic, FontWeight.Bold, FontStyle.Italic)
+    Font(R.font.noto_serif_regular, FontWeight.Normal),
+    Font(R.font.noto_serif_bold, FontWeight.Bold),
+    Font(R.font.noto_serif_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.noto_serif_bold_italic, FontWeight.Bold, FontStyle.Italic)
 )
 
 val typography = Typography(
