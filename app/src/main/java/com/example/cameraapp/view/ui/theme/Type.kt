9b12745d3b655398a3b939e6202bd9e98200d2d6
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-21 07:54:17 -0500

    feat: add typography

--- app/src/main/java/com/example/cameraapp/view/ui/theme/Type.kt
+++ app/src/main/java/com/example/cameraapp/view/ui/theme/Type.kt
@@ -1,28 +1,55 @@
-package com.example.cameraapp.ui.theme
+package com.example.cameraapp.view.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+
+val NotoSerif = FontFamily(
+    Font(font.noto_serif_regular, FontWeight.Normal),
+    Font(font.noto_serif_bold, FontWeight.Bold),
+    Font(font.noto_serif_italic, FontWeight.Light, FontStyle.Italic),
+    Font(font.noto_serif_bold_italic, FontWeight.Bold, FontStyle.Italic)
+)
+
+val typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = NotoSerif,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    titleMedium = TextStyle(
+        fontFamily = NotoSerif,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontFamily = NotoSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontFamily = NotoSerif,
+        fontSize = 10.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.15.sp,
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Light,
+        fontFamily = NotoSerif,
+        fontSize = 6.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.15.sp
     )
-    */
 )
