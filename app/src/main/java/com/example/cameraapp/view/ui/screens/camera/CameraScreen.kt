1f9111120694f3a28182caf71f3d090648741ac6
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-21 19:02:26 -0500

    feat: add simple camera screen

---
+++ app/src/main/java/com/example/cameraapp/view/ui/screens/camera/CameraScreen.kt
@@ -0,0 +1,45 @@
+package com.example.cameraapp.view.ui.screens.camera
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.cameraapp.R
+import com.example.cameraapp.view.ui.theme.CameraAppTheme
+
+@Composable
+fun CameraScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.teal_700))
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Camera Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CameraAppTheme {
+        CameraScreen()
+    }
+}
