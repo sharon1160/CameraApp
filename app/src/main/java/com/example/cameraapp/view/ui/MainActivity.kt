233e776903ddfdf763d1caab77a12afc996ef262
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-21 19:05:39 -0500

    feat: add home screen to main activity

--- app/src/main/java/com/example/cameraapp/view/ui/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/view/ui/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.cameraapp.view.ui.screens.home.HomeScreen
 import com.example.cameraapp.view.ui.theme.CameraAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,29 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CameraAppTheme {
-                // A surface container using the 'background' colo
-                // r from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
-    }
-
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CameraAppTheme {
-        Greeting("Android")
     }
 }
