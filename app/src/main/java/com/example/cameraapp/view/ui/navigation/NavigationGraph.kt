6b028e61186403d0749295112d0cc1ebc05d6c76
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-23 10:30:16 -0500

    feat: add gallery view model

--- app/src/main/java/com/example/cameraapp/view/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/cameraapp/view/ui/navigation/NavigationGraph.kt
@@ -6,12 +6,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.cameraapp.view.ui.screens.camera.CameraScreen
 import com.example.cameraapp.view.ui.screens.gallery.GalleryScreen
+import com.example.cameraapp.viewmodel.GalleryViewModel
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
     NavHost(navController, startDestination = NavigationItem.Gallery.screen_route) {
         composable(NavigationItem.Gallery.screen_route) {
-            GalleryScreen()
+            val galleryViewModel = GalleryViewModel()
+            GalleryScreen(galleryViewModel)
         }
         composable(NavigationItem.Camera.screen_route) {
             CameraScreen()
