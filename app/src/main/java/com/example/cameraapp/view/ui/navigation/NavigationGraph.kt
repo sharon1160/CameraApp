31a439f7db1f82d16faf502bf200ec045683d5f3
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-23 15:36:00 -0500

    feat: add camera view model

--- app/src/main/java/com/example/cameraapp/view/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/cameraapp/view/ui/navigation/NavigationGraph.kt
@@ -1,11 +1,13 @@
 package com.example.cameraapp.view.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.cameraapp.view.ui.screens.camera.CameraScreen
 import com.example.cameraapp.view.ui.screens.gallery.GalleryScreen
+import com.example.cameraapp.viewmodel.CameraViewModel
 import com.example.cameraapp.viewmodel.GalleryViewModel
 
 @Composable
@@ -16,7 +18,9 @@ fun NavigationGraph(navController: NavHostController) {
             GalleryScreen(galleryViewModel)
         }
         composable(NavigationItem.Camera.screen_route) {
-            CameraScreen()
+            val viewModelStoreOwner = navController.getViewModelStoreOwner(navController.graph.id)
+            val cameraViewModel = ViewModelProvider(viewModelStoreOwner)[CameraViewModel::class.java]
+            CameraScreen(cameraViewModel)
         }
     }
 }
