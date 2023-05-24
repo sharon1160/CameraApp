46a53ced6747ad6484219bb6aee82ead616d11ed
Sharon Chullunquia Rosas <schullunquia@unsa.edu.pe>
2023-05-24 12:47:36 -0500

    feat: add function to take photos

--- app/src/main/java/com/example/cameraapp/viewmodel/CameraViewModel.kt
+++ app/src/main/java/com/example/cameraapp/viewmodel/CameraViewModel.kt
@@ -1,19 +1,25 @@
 package com.example.cameraapp.viewmodel
 
+import android.Manifest
 import android.app.Application
 import android.content.ContentResolver
 import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.provider.MediaStore
 import android.util.Log
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
 import com.example.cameraapp.view.ui.screens.camera.CameraUiState
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,10 +27,13 @@ class CameraViewModel(application: Application): AndroidViewModel(application) {
 
     private val _uiState = MutableStateFlow(CameraUiState())
     val uiState = _uiState.asStateFlow()
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     private val contentResolver: ContentResolver = application.contentResolver
 
-    fun takePhoto(imageCapture: ImageCapture) {
+    fun takePhoto(imageCapture: ImageCapture, context: Context) {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
         val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
         val contentValues = ContentValues().apply {
             put(MediaStore.MediaColumns.DISPLAY_NAME, name)
@@ -33,27 +42,44 @@ class CameraViewModel(application: Application): AndroidViewModel(application) {
                 put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
             }
         }
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(
-            contentResolver,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            contentValues
-        ).build()
 
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(getApplication()),
-            object : ImageCapture.OnImageSavedCallback {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
 
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val msg = "Photo capture succeeded ${output.savedUri}"
-                    Log.d(TAG, msg)
-                }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+            val photoLocation = ImageCapture.Metadata().apply {
+                this.location = location
             }
-        )
+            val outputOptions = ImageCapture.OutputFileOptions.Builder(
+                contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            ).setMetadata(photoLocation).build()
+
+            imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(getApplication()),
+                object : ImageCapture.OnImageSavedCallback {
+
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e(TAG, "Photo capture failed ${exc.message}", exc)
+                    }
+
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        val msg = "Photo capture succeeded ${output.savedUri}"
+                        Log.d(TAG, msg)
+                    }
+                }
+            )
+        }
     }
 
     companion object {
